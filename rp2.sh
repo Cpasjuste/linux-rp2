a3a9ff077fa91f64f6a7968285aaebe48f985187
cpasjuste <cpasjuste@gmail.com>
2022-10-04 08:02:45 +0200

    rp2: update debug script

--- rp2.sh
+++ rp2.sh
@@ -7,7 +7,7 @@ make -j16 ARCH="arm" CROSS_COMPILE=${RETROROOT_CROSS_PREFIX}
 exit 0
 
 mkbootimg \
-  --kernel "arm/arm/boot/zImage-dts" \
+  --kernel "arch/arm/boot/zImage-dtb" \
   --ramdisk "rp2-initramfs-linux.img" \
   --base "0x80000000" \
   --second_offset "0x00f00000" \
@@ -17,4 +17,6 @@ mkbootimg \
   --tags_offset "0x0e000000" \
   --pagesize "2048" \
   -o rr-boot.img
- 
+
+mtk_flash -d rp2-da.bin -R -a 0x1d20000 -l 0xDD3000 -F rr-boot.img
+
